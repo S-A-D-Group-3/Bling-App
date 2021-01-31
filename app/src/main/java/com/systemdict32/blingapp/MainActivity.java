4750d3c7f89956917041e0659effcaeadd8f5e51
dahren02 <drcamacho@iskolarngbayan.pup.edu.ph>
2021-02-01 00:40:55 +0800

    Added splashscreen/ stable version on gradle

--- app/src/main/java/com/systemdict32/blingapp/MainActivity.java
+++ app/src/main/java/com/systemdict32/blingapp/MainActivity.java
@@ -1,19 +1,8 @@
 package com.systemdict32.blingapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 

d6e9a21ba36973d96fcba11cadd2496decf79aa2
dahren02 <drcamacho@iskolarngbayan.pup.edu.ph>
2021-02-01 19:42:20 +0800

    added Login Screen with transition

--- app/src/main/java/com/systemdict32/blingapp/splashscreen.java
+++ app/src/main/java/com/systemdict32/blingapp/splashscreen.java
@@ -2,9 +2,12 @@ package com.systemdict32.blingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -41,17 +44,15 @@ public class splashscreen extends AppCompatActivity {
         introtwo.setAnimation(bottomAnim);
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
+        new Handler().postDelayed( ()  -> {
+                Intent intent = new Intent(splashscreen.this, Login.class);
+             Pair[] pairs = new Pair[2];
+            pairs[0] = new Pair<View, String>(image, "logo_image");
+            pairs[1] = new Pair<View, String>(introwan, "logo_text");
 
-            public void run(){
-                Intent intent = new Intent(splashscreen.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(splashscreen.this, pairs);
+            startActivity(intent,options.toBundle());
 
-
-
-            }
         }, SPLASH_SCREEN);
 
 

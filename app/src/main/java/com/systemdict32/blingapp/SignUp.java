6b3a2b3898e1af632e73f61395cc613937a20cde
dahren02 <2.dahrencamacho@gmail.com>
2021-04-10 06:00:24 +0800

    TESTING FIX FOR MERGING OF DIFFERENT COMMITS

--- app/src/main/java/com/systemdict32/blingapp/SignUp.java
+++ app/src/main/java/com/systemdict32/blingapp/SignUp.java
@@ -1,5 +1,6 @@
 package com.systemdict32.blingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActivityOptions;
@@ -9,61 +10,82 @@ import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
     //Variables natin
-    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    TextInputLayout  regEmail, regPassword;
     Button goBack, regBtn, regToLoginBtn;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    FirebaseAuth firebaseAuth;
+    ProgressBar progressBar;
+
+
+
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
         //hooks to signup.xml
-        regName = findViewById(R.id.reg_fullname);
-        regUsername = findViewById(R.id.reg_username);
+
         regEmail = findViewById(R.id.reg_email);
-        regPhoneNo = findViewById(R.id.reg_phoneNo);
         regPassword = findViewById(R.id.reg_password);
         regBtn =  findViewById(R.id.reg_btn);
         regToLoginBtn =  findViewById(R.id.goback);
+        progressBar = findViewById(R.id.progressBar);
+        firebaseAuth = FirebaseAuth.getInstance();
 
         regBtn.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view){
-            rootNode = FirebaseDatabase.getInstance("https://bling-230eb-default-rtdb.firebaseio.com/");
-            reference = rootNode.getReference("users");
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String sEmail = regEmail.getEditText().getText().toString().trim();
+                String sPass = regPassword.getEditText().getText().toString().trim();
+                firebaseAuth.createUserWithEmailAndPassword(sEmail, sPass)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    firebaseAuth.getCurrentUser().sendEmailVerification()
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if(task.isSuccessful()){
+                                                        Toast.makeText(SignUp.this, "Registered successfully. Please check your email for verification",
+                                                                Toast.LENGTH_LONG).show();
+                                                        regEmail.getEditText().setText("");
+                                                        regPassword.getEditText().setText("");
+                                                        startActivity(new Intent(SignUp.this, Login.class));
+                                                    }else{
+                                                        Toast.makeText(SignUp.this,  task.getException().getMessage(),
+                                                                Toast.LENGTH_LONG).show();
+                                                    }
 
-            //validation
-                if(!validateName() | !validateUserName() | !validateEmail() | !validatePhone()| !validatePassword()){
-                    return;
-                }
-
-            //get values
-                String name = regName.getEditText().getText().toString();
-                String username = regUsername.getEditText().getText().toString();
-                String email = regEmail.getEditText().getText().toString();
-                String phoneNo = regPhoneNo.getEditText().getText().toString();
-                String password = regPassword.getEditText().getText().toString();
-
-            UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
-
-            reference.child(name).setValue(helperClass);
-
-                Intent intent = new Intent(SignUp.this, Login.class);
-                startActivity(intent);
-
+                                                }
+                                            });
+                                } else {
+                                    Toast.makeText(SignUp.this, task.getException().getMessage(),
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
             }
-
 
 
         });//register button method end
@@ -88,42 +110,10 @@ public class SignUp extends AppCompatActivity {
         }//onCreate method end
 
 
-        private boolean validateName(){
-            String val = regName.getEditText().getText().toString();
-            if(val.isEmpty()){
-                regName.setError("This part must be filled up");
-                return false;
-            }
-            else{
-                regName.setError(null);
-                regName.setErrorEnabled(false);
-                return true;
-            }
-        }
 
-    private boolean validateUserName(){
-        String val = regUsername.getEditText().getText().toString();
-        String noWhiteSpace ="\\A\\w{4,20}\\z";
-        if(val.isEmpty()){
-            regUsername.setError("This part must be filled up");
-            return false;
-        }else if (val.length()>=15){
-            regUsername.setError("Username too long");
-            return false;
 
-        }else if(!val.matches(noWhiteSpace)){
-            regUsername.setError("Please don't leave spaces");
-            return false;
 
-        }
-        else{
-            regUsername.setError(null);
-            regUsername.setErrorEnabled(false);
-            return true;
-        }
-    }
-
-    private boolean validateEmail(){
+   /** private boolean validateEmail(){
         String val = regEmail.getEditText().getText().toString();
         String emailPattern = "[a-zA-z0-9._-]+@[a-z]+\\.+[a-z]+";
         if(val.isEmpty()){
@@ -139,20 +129,10 @@ public class SignUp extends AppCompatActivity {
             return true;
         }
     }
+    **/
 
-    private boolean validatePhone(){
-        String val = regPhoneNo.getEditText().getText().toString();
-        if(val.isEmpty()){
-            regPhoneNo.setError("This part must be filled up");
-            return false;
-        }
-        else{
-            regPhoneNo.setError(null);
-            return true;
-        }
-    }
 
-    private boolean validatePassword(){
+   /** private boolean validatePassword(){
         String val = regPassword.getEditText().getText().toString();
         String passVal = "^"+
                 "(?=.*[a-zA-Z])"+
@@ -171,6 +151,7 @@ public class SignUp extends AppCompatActivity {
             return true;
         }
     }
+    **/
 
 
 }

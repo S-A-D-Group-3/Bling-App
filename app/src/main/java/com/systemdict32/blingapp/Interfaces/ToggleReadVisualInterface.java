1f0935df12096dd9c816a26b6aba8f74edad4e30
Jan Eleven Salvador <janelevensalvador25@gmail.com>
2021-04-21 16:44:33 +0800

    added visual read toggle button

---
+++ app/src/main/java/com/systemdict32/blingapp/Interfaces/ToggleReadVisualInterface.java
@@ -0,0 +1,8 @@
+package com.systemdict32.blingapp.Interfaces;
+
+import androidx.fragment.app.Fragment;
+
+public interface ToggleReadVisualInterface {
+    public Fragment getVisualFragment();
+    public Fragment getReadFragment();
+}

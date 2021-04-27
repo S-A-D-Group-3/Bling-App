8f1a24e430d33c0f4b7d008fb89419e570207580
dahren02 <2.dahrencamacho@gmail.com>
2021-04-28 05:17:35 +0800

    4-28-21 :) Profile Pic update thru choosing file ( not finished)

--- app/src/main/java/com/systemdict32/blingapp/Fragments/MyAccountFragment.java
+++ app/src/main/java/com/systemdict32/blingapp/Fragments/MyAccountFragment.java
@@ -1,21 +1,35 @@
 package com.systemdict32.blingapp.Fragments;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
@@ -26,8 +40,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.systemdict32.blingapp.Dashboard;
+import com.systemdict32.blingapp.Login;
 import com.systemdict32.blingapp.R;
 
+import java.net.URI;
 import java.util.concurrent.Executor;
 
 import es.dmoral.toasty.Toasty;
@@ -41,10 +63,13 @@ public class MyAccountFragment extends Fragment implements Executor {
 
 
     TextView fullname, email;
+    Button dismiss;
     FirebaseFirestore fStore;
     FirebaseAuth firebaseAuth;
+    StorageReference storageReference;
     ImageView picture;
     String userId;
+
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -96,10 +121,19 @@ public class MyAccountFragment extends Fragment implements Executor {
         fullname = view.findViewById(R.id.profile_fname);
         email = view.findViewById(R.id.profile_email);
         picture = view.findViewById(R.id.profile_pic);
-
+        dismiss = view.findViewById(R.id.button_dis);
         firebaseAuth = FirebaseAuth.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        StorageReference profileRefence = storageReference.child("users/"+firebaseAuth.getUid()+"/profile_image.jpg");
+        profileRefence.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(picture);
+            }
+        });
         fStore = FirebaseFirestore.getInstance();
         userId = firebaseAuth.getCurrentUser().getUid();
+
 
         fStore.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
@@ -117,9 +151,119 @@ public class MyAccountFragment extends Fragment implements Executor {
             }
         });
 
+        picture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // your code here
+                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case DialogInterface.BUTTON_POSITIVE:
+                                Dialog builder = new Dialog(getActivity());
+                                builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                                builder.getWindow().setBackgroundDrawable(
+                                        new ColorDrawable(Color.GRAY));
+                                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                                    @Override
+                                    public void onDismiss(DialogInterface dialogInterface) {
+                                    }
+                                });
+                                Uri imgUri = Uri.parse("android.resource://com.systemdict32.blingapp/drawable/default_user");
+                                 picture = new ImageView(getActivity());
+                               picture.setImageURI(imgUri);
+                                builder.addContentView(picture, new RelativeLayout.LayoutParams(
+                                        ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.MATCH_PARENT));
+                                builder.show();
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        switch (which) {
+                                            case DialogInterface.BUTTON_POSITIVE:
+                                                break;
+
+                                            case DialogInterface.BUTTON_NEGATIVE:
+                                                Intent openFiles = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                                                startActivityForResult(openFiles, 1000);
+
+
+                                                break;
+                                        }
+                                    }
+                                };
+
+
+
+                                AlertDialog.Builder builder2 = new AlertDialog.Builder(getActivity());
+                                builder2.create();
+                                builder2.setCancelable(false);
+                                builder2.setMessage("Set Profile Photo").setPositiveButton("Use Camera", dialogClickListener)
+                                        .setNegativeButton("Choose from Files", dialogClickListener).show();
+                                break;
+                        }
+                    }
+                };
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.create();
+                builder.setCancelable(false);
+                builder.setMessage("Profile Photo Options").setPositiveButton("View Photo", dialogClickListener)
+                        .setNegativeButton("Update", dialogClickListener).show();
+
+
+            }
+
+        });
+
+
+
+
         // Inflate the layout for this fragment
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data){
+        MyAccountFragment.super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000) {
+            if(resultCode == Activity.RESULT_OK) {
+
+                Uri imgUri = data.getData();
+               // picture.setImageURI(imgUri);
+                uploadImageToFirebase(imgUri);
+
+
+
+            }
+
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imgUri) {
+
+        StorageReference fileReference = storageReference.child("users/"+firebaseAuth.getUid()+"/profile_image.jpg");
+        fileReference.putFile(imgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(picture);
+                    }
+                });
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toasty.error(getActivity(), "Failed to Update", Toast.LENGTH_SHORT, true).show();
+            }
+        });
+    }
+
 
     @Override
     public void execute(Runnable command) {
